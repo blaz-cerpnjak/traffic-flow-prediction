d5d486694a16729f3b97aadc91e497719d5450c3
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-06-01 10:41:18 +0200

    Added swagger docs, api key and Dockerfile for predictino api

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# Use an official Python runtime as a parent image
+FROM python:3.11-slim
+
+ENV PATH="${PATH}:/root/.poetry/bin"
+ENV PYTHONPATH "${PYTHONPATH}:/app/src"
+
+# Set the working directory
+WORKDIR /app
+
+# Install dependencies
+RUN pip install pandas requests uvicorn fastapi onnxruntime python-dotenv pymongo mlflow onnx
+
+# Copy the rest of the application code
+COPY ./src /app/src
+COPY ./src/utils /app/src/utils
+COPY ./data /app/data
+COPY .env /app/.env
+
+# Create a README file
+RUN echo "Welcome to my Docker image!" > /app/README.md
+
+# Expose the application port
+EXPOSE 8000
+
+WORKDIR /app/src/serve
+
+# Command to run the application
+#CMD ["uvicorn", "src.serve.api:app", "--host", "0.0.0.0", "--port", "8000"]
+#CMD ["python", "-m", "src.serve.api"]
+CMD ["python", "api.py"]
--- src/serve/api.py
+++ src/serve/api.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Header, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from src.serve.utils import models_service, mlflow_service
 from src.serve.utils.predict_travel_times_service import predict_travel_times_for_next_hours
@@ -11,10 +11,20 @@ import onnx
 import joblib
 from pydantic import BaseModel
 from typing import Optional
+from dotenv import load_dotenv
+from fastapi.security import OAuth2PasswordBearer
 
 scalers = {}
 
-app = FastAPI()
+app = FastAPI(
+    title="Traffic Flow Prediction API",
+    description="API for predicting travel times (in minutes) and vehicle counts (per hour) for different highway locations in Slovenia.",
+    version="1.0.0",
+    contact={
+        "name": "Blaž Čerpnjak",
+        "url": "https://github.com/blaz-cerpnjak",
+    },
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,25 +34,40 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/ping")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="")
+
+def verify_api_key(token: str = Depends(oauth2_scheme)):
+    if token != os.getenv("API_KEY"):
+        raise HTTPException(status_code=403, detail="Forbidden")
+
+@app.get("/ping", summary="Ping the server", response_description="pong response")
 async def ping():
+    """Check if the server is running."""
     return "pong"
 
-@app.get("/travel-times/predict/{location_name}/{hours}")
-async def predict_travel_times_service(location_name: str, hours: int):
-    if location_name not in scalers:
+@app.get("/api/v1/travel-times/predict/{route}/{hours}", summary="Predict travel times for a specific location", response_description="Travel time predictions")
+async def predict_travel_times_service(route: str, hours: int):
+    """
+    Predict travel times for a specific location and number of hours.
+    - **route**: The name of the route (LJ_KP, KP_LJ...). 
+    - **hours**: The number of hours to predict.
+    """
+    if route not in scalers:
         raise HTTPException(status_code=404, detail="Location not found")
 
-    model_scalers = scalers[location_name]
+    model_scalers = scalers[route]
     if model_scalers is None:
         raise HTTPException(status_code=404, detail="Location not found")
 
-    predictions = predict_travel_times_for_next_hours(f'{location_name}_model.onnx', model_scalers, location_name, hours)
+    predictions = predict_travel_times_for_next_hours(f'{route}_model.onnx', model_scalers, route, hours)
     print(predictions)
     return {'predictions': predictions}
 
-@app.get("/travel-times/predict")
+@app.get("/api/v1/travel-times/predict", summary="Predict travel times for all locations", response_description="Travel time predictions for all locations")
 async def predict_travel_times_service():
+    """
+    Predict travel times for all locations.
+    """
     predictions = []
 
     for location_name in LOCATIONS.keys():
@@ -58,17 +83,24 @@ async def predict_travel_times_service():
 
     return {'predictions': predictions}
 
-@app.get("/travel-times/model-data/{location_name}")
-async def predict_travel_times_service(location_name):
-    run_id = mlflow_service.get_latest_travel_time_production_model_run_id(location_name)
+@app.get("/api/v1/travel-times/model-data/{location_name}", summary="Get travel time model data", response_description="Model data for the travel time prediction")
+async def predict_travel_times_service(route: str):
+    """
+    Retrieve the model data for a specific location.
+    - **route**: The name of the route.
+    """
+    run_id = mlflow_service.get_latest_travel_time_production_model_run_id(route)
     if run_id is None:
         raise HTTPException(status_code=404, detail="Model data not found")
     
     model_data = mlflow_service.get_model_data(run_id)
     return {'data': model_data}
 
-@app.get("/vehicle-counter/routes")
+@app.get("/api/v1/vehicle-counter/routes", summary="Get vehicle counter routes", response_description="List of vehicle counter routes")
 async def get_vehicle_counter_routes():
+    """
+    Retrieve all vehicle counter routes.
+    """ 
     routes = []
 
     for location, directions in HIGHWAY_LOCATIONS.items():
@@ -82,8 +114,13 @@ async def get_vehicle_counter_routes():
 
     return {'routes': routes}
 
-@app.get("/vehicle-counter/model-data/{location_name}/{direction}")
-async def predict_travel_times_service(location_name: str, direction: str):
+@app.get("/api/v1/vehicle-counter/model-data/{location_name}/{direction}", summary="Get vehicle counter model data", response_description="Model data for the vehicle counter")
+async def get_vehicle_counter_model_data(location_name: str, direction: str):
+    """
+    Retrieve the model data for a specific location and direction.
+    - **location_name**: The name of the location.
+    - **direction**: The direction of the vehicle counter.
+    """
     run_id = mlflow_service.get_latest_vehicle_counter_production_model_run_id(location_name, direction)
     if run_id is None:
         raise HTTPException(status_code=404, detail="Model data not found")
@@ -91,8 +128,14 @@ async def predict_travel_times_service(location_name: str, direction: str):
     model_data = mlflow_service.get_model_data(run_id)
     return {'data': model_data}
 
-@app.get("/vehicle-counter/predict/{location_name}/{direction}/{hours}")
+@app.get("/api/v1/vehicle-counter/predict/{location_name}/{direction}/{hours}", summary="Predict vehicle count for a specific location and direction", response_description="Vehicle count predictions")
 async def predict_vehicle_counter_service(location_name: str, direction: str, hours: int):
+    """
+    Predict vehicle counts for a specific location, direction, and number of hours.
+    - **location_name**: The name of the traffic (camera) sensor location.
+    - **direction**: The direction of the vehicle counter.
+    - **hours**: The number of hours to predict.
+    """
     if not os.path.exists(location_name):
         os.makedirs(location_name)
     
@@ -143,26 +186,44 @@ class ExperimentSearchRequest(BaseModel):
     type: str
     name: Optional[str] = None
 
-@app.post("/vehicle-counter/search-experiments")
+@app.post("/api/v1/vehicle-counter/search-experiments", summary="Search Mlflow experiments", response_description="List of Mlflow experiments", dependencies=[Depends(verify_api_key)])
 async def search_experiments(request: ExperimentSearchRequest):
+    """
+    ! Requires API key !
+    Search for experiments.
+    - **type**: The type of experiment.
+    - **name**: Optional name of the experiment.
+    """
     experiments = mlflow_service.search_experiments(int(request.type), request.name)
     return {'experiments': experiments}
 
-@app.get("/vehicle-counter/runs/{experiment_id}")
+@app.get("/api/v1/vehicle-counter/runs/{experiment_id}", summary="Get Mlflow runs by experiment ID", response_description="List of Mlflow runs")
 async def get_runs_by_experiment_id(experiment_id: int):
+    """
+    Retrieve runs by experiment ID.
+    - **experiment_id**: The ID of the experiment.
+    """
     runs = mlflow_service.get_runs_by_experiment_id(experiment_id)
     return {'runs': runs}
 
 class ModelSearchRequest(BaseModel):
     name: Optional[str] = None
 
-@app.post("/models")
+@app.post("/api/v1/models", summary="Get registered models", response_description="List of models in Mlflow registry")
 async def get_registered_models(request: ModelSearchRequest):
+    """
+    Retrieve registered models.
+    - **name**: Optional name of the model.
+    """
     models = mlflow_service.get_registered_models(request.name)
     return {'models': models}
 
-@app.get("/models/{model_name}")
+@app.get("/api/v1/models/{model_name}", summary="Get model versions by name", response_description="List of model versions")
 def get_model_versions_by_name(model_name):
+    """
+    Retrieve model versions by model name.
+    - **model_name**: The name of the model.
+    """
     model_versions = mlflow_service.get_model_version_by_name(model_name)
     return model_versions
 
@@ -171,8 +232,15 @@ class ChangeModelStageRequest(BaseModel):
     version: str
     stage: str
 
-@app.post("/change-model-stage")
+@app.post("/api/v1/change-model-stage", summary="Change model stage", response_description="Confirmation message", dependencies=[Depends(verify_api_key)])
 async def change_model_stage(request: ChangeModelStageRequest):
+    """
+    ! Requires API key !
+    Change the stage of a model.
+    - **model_name**: The name of the model.
+    - **version**: The version of the model.
+    - **stage**: The new stage of the model.
+    """
     mlflow_service.change_model_stage(request.model_name, request.version, request.stage)
     return {'message': 'Model stage changed'}
 
@@ -180,9 +248,11 @@ def load_production_models():
     print("Loading production models...")
     for location_name in LOCATIONS.keys():
         model, scalers[location_name] = models_service.fetch_current_travel_time_model(location_name)
+        print(f'Loaded model for location: {location_name}')
         onnx.save(model, f'{location_name}_model.onnx')
     
 if __name__ == "__main__":
     import uvicorn
+    load_dotenv()
     load_production_models()
     uvicorn.run(app, host="0.0.0.0", port=8000)
