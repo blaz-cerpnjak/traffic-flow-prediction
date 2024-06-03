397b2c7a0e24478c88f0fbdffd8ede45846a8774
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-06-03 12:12:51 +0200

    Removed axios base url

--- src/client/src/axios/index.js
+++ src/client/src/axios/index.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 import router from '@/router'
 
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-});
+const axiosInstance = () => axios.create({});
 
 axiosInstance.interceptors.request.use(
     config => {
