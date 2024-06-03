3d340f932a1c7c5dc0534bd0c44b33a466ea132d
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-06-03 11:46:29 +0200

    Added server proxy config

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -7,6 +7,16 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig(() => {
     return {
         plugins: [vue()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'https://traffic-flow-prediction-api-0-1-0.onrender.com/api',
+                    changeOrigin: true,
+                    secure: true, // Set to true if your backend uses HTTPS
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
+            },
+        },
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
