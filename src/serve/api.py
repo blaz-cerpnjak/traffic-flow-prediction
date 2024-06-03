c4a59826e240ac596c9fa502600bd910286b1538
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-06-03 12:21:39 +0200

    Fixed cors config

--- src/serve/api.py
+++ src/serve/api.py
@@ -26,10 +26,14 @@ app = FastAPI(
     },
 )
 
+origins = [
+    "http://localhost:5173",
+    "https://traffic-flow-prediction-web-app.onrender.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
